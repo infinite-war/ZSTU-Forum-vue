09635792855b1f8b5fc7104677161bd415315e17
infinite-war <a969557028@gmail.com>
2022-05-16 19:33:08 +0800

    Initial commit

---
+++ myvue/src/components/releasepost.vue
@@ -0,0 +1,110 @@
+<template>
+  <body>
+  <h1>发布新帖子</h1>
+  <el-form ref="form" :model="form" label-width="80px" :rules="rules">
+    <el-form-item label="帖子标题" prop="title">
+      <el-input v-model="form.title" ></el-input>
+    </el-form-item>
+    <!--进行列表渲染，数据全部存在Vuex的全局静态变量里，修改分区数只需要修改store/index.js的静态数据-->
+    <el-form-item label="活动分类" prop="category">
+      <el-select  filterable v-model="form.category" placeholder="请选择分类">
+        <el-option
+          v-for="item in this.$store.state.homepageClass"
+          :key="item.home"
+          :value="item.typeId"
+          :label="item.home"
+        >
+        </el-option>
+      </el-select>
+    </el-form-item>
+<!--    <el-form-item label="帖子内容" prop="content">-->
+<!--      <el-input type="textarea" v-model="form.content" :rows="20"></el-input>-->
+<!--    </el-form-item>-->
+    <div label="帖子内容" prop="content">
+      <mavon-editor v-model="form.content" :ishljs="true"/>
+    </div>
+
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">提交</el-button>
+      <el-button @click="returnMain">取消</el-button>
+    </el-form-item>
+  </el-form>
+
+  </body>
+
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+
+  data() {
+    return {
+      postsId:'',
+      form: {
+        title: '',
+        category: '',
+        content: ''
+      },
+      rules:{
+        title:[{required : true,message: '请输入标题',trigger:'blur'}],
+        content:[{required : true,message: '请输入内容',trigger:'blur'}],
+        category:[{required : true,message: '请选择分类',trigger:'change'}],
+      }
+    }
+  },
+  created() {
+    if(this.$store.state.localid===''||this.$store.state.localid===undefined){
+      alert("未登录，无法发布")
+      this.$router.push('/login')
+    }
+  },
+  methods: {
+    onSubmit() {
+      //alert('submit!');
+      if(this.form.content===''||this.form.title===''||this.form.category===""){
+        alert("有未填写项，无法发布")
+      }
+      else if(this.$store.state.localid===''||this.$store.state.localid===undefined){
+        alert("未登录，无法发布")
+      }
+      else {
+        const self = this;
+        self.$axios({
+          method:'post',
+          url:'/post',
+          data:{
+            category:self.form.category,
+            title:self.form.title,
+            content:self.form.content
+          }
+        })
+        .then(res=>{
+          if(res.data.data.flag===true) {
+           alert(res.data.message)
+            console.log(res)
+            self.postsId=res.data.postsId
+          }
+          else {
+            alert(res.data.message)
+            console.log(res)
+          }
+
+        })
+      }
+    },
+    returnMain(){
+
+      this.$router.push("homepageone?typeId=1&page=1");
+      //测试路由跳转
+      //this.$router.push({ name: 'homepageone', params: { typeId: '1' ,page:'1'} })
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ myvue/src/router/index.js
@@ -0,0 +1,103 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+import Login from "../components/Login";
+import Index from '../components/Index'
+import welcome from "../components/welcome";
+import user from "../components/user";
+import {use} from "element-ui";
+import {drag} from '../assets/JS/welcome'
+import show01 from "../components/show01";
+import show02 from "../components/show02";
+import show00 from "../components/show00";
+import show03 from "../components/show03";
+import releasepost from "../components/releasepost";
+import homepageone from "../components/homepageone";
+import posts from "../components/posts";
+import personal from "../components/personal";
+import userProfile from "../components/userProfile";
+Vue.use(Router);
+
+
+export default new Router({
+  routes:[
+
+    {
+      //这里的Login是上面import的的Login
+      name:'login',
+      path:'/login',
+      component:Login
+    },
+    //全局跳转到登录页
+    //先登录，不然进不去主页面
+    {
+      path:'/',
+      redirect:"/index"
+    },
+    //在主页面实现基本功能
+    //路由跳转就是匹配此处的path规则，匹配到就使用path对应的组件，渲染到进行中的route-view处
+    {
+      name:'index',
+      path:'/index',
+      component:Index,
+      redirect:'/welcome',
+      children:[
+        {
+          name:"welcome",
+          path:'/welcome',
+          component:welcome
+        },
+        {
+          name:'user',
+          path:'/user',
+          component:user
+        },
+        {
+          name:'releasepost',
+          path:'/releasepost',
+          component:releasepost
+        },
+        {
+          name:'homepageone',
+          path:'/homepageone',
+          component:homepageone
+        },
+        {
+          name:'posts',
+          path:'/posts/:postsId',
+          component:posts
+        },
+        {
+          name:'personal',
+          path:'/personal/:id',
+          component:personal
+        },
+        {
+          name:'userProfile',
+          path:'/userprofile',
+          component:userProfile
+        }
+      ]
+    },
+    {
+      name:'show00',
+      path:'/show00',
+      component:show00
+    },
+    {
+      name:'show01',
+      path:'/show01',
+      component:show01
+    },
+    {
+      name:'show02',
+      path:'/show02',
+      component:show02
+    },
+    {
+      name:'show03',
+      path:'/show03',
+      component:show03
+    },
+  ]
+});
